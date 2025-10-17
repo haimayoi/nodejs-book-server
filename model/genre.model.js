f1c7c4c479f028b315b4f319e6f16702e86b3f8b
Hải Vân <k34ctqer@gmail.com>
2025-10-17 19:46:19 +0700

    Modify

--- model/genre.model.js
+++ model/genre.model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const genreSchema = new mongoose.Schema({
-    name: { type: String, require: true}
+    name: { 
+        type: String, 
+        require: true
+    }
 });
 
 module.exports = mongoose.model('genres', genreSchema);
